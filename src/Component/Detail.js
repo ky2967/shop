5959a57c33cd3d511c9b89c40ae0cf43eafd61ba
ky2967 <ky6015@naver.com>
2022-02-16 17:57:41 +0900

    Lifecycle Hook (옛날사람) useEffect (요즘사람)

--- src/Component/Detail.js
+++ src/Component/Detail.js
@@ -1,6 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import {useHistory, useParams} from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+
+// styled : CSS를 미리 입힌 컴포넌트
+let 박스 = styled.div`
+  padding: 20px;
+`;
+
+let 제목 = styled.h4`
+  font-size: 25px;
+  color: ${(props) => props.색상};
+`;
 
 function Detail(props) {
   let history = useHistory();
@@ -8,11 +20,34 @@ function Detail(props) {
 
   let [shoes, setShoes] = useState(props.shoes);
 
+  const [showAlert, setShowAlert] = useState(true);
+
   const sho = shoes.find((item) => item.id === parseInt(id));
   // Ajax를 이용하여 데이터 추출 가능  ex) {id : 0}
 
+  // 컴포넌트 렌더링 시 실행
+  useEffect(() => {
+    // 특정시간이 지나서 코드 실행
+    let timer = setTimeout(() => {
+      setShowAlert(false);
+    }, 2000);
+
+    // 컴포넌트가 반응될 때 코드 실행
+    // return function func(() => {
+
+    // });
+  });
+
   return (
     <Container>
+      <박스>
+        <제목 className="red">Detail</제목>
+      </박스>
+      {showAlert ? (
+        <div className="my-alert-red">
+          <p>재고가 얼마 남지않았습니다.</p>
+        </div>
+      ) : null}
       <Row>
         <Col>
           <img
